0167bf606326cf14a607ad0e6721de6efae004cc
HGarry <147743465+HGarry@users.noreply.github.com>
2026-09-04 02:17:27 +0630

    feat:add lazy loading for better perfomance

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,27 @@
-"use client";
+import dynamic from "next/dynamic";
+import Image from "next/image";
+
 import HeroSection from "@/components/HeroSection";
-import Skills from "@/components/Skills";
-import TestimonialCarousel from "@/components/Projects";
-import Contact from "@/components/Contact";
+
+const Skills = dynamic(() => import("@/components/Skills"));
+const TestimonialCarousel = dynamic(() => import("@/components/Projects"));
+const Contact = dynamic(() => import("@/components/Contact"));
 
 export default function Home() {
   return (
     <div className="w-full">
-      <div
-        style={{ backgroundImage: "url('/images/hero.jpg')" }}
-        className="w-screen h-screen bg-cover bg-no-repeat fixed top-0 left-0 z-[-1]"
-      ></div>
+      <div className="fixed top-0 left-0 w-full h-screen z-[-1]">
+        <Image
+          src="/images/hero.jpg"
+          alt="Hero Background"
+          fill
+          priority
+          quality={70}
+          sizes="100vw"
+          className="object-cover"
+        />
+      </div>
+
       <HeroSection />
       <Skills />
       <TestimonialCarousel />
